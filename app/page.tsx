d9b20b06064f84e6a424bacebabf625e07a84fbe
David Ernst <git@dsernst.com>
2023-12-19 19:17:13 -0800

    Delete <ConnectSupabaseSteps /> from tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,15 @@
 import { Logo } from '../components/Logo'
 import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
 import SignUpUserSteps from '@/components/SignUpUserSteps'
 import Header from '@/components/Header'
-import { cookies } from 'next/headers'
 
 export default async function Index() {
-  const cookieStore = cookies()
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  const isSupabaseConnected = canInitSupabaseClient()
-
   return (
     <div className="flex flex-col items-center flex-1 w-full gap-20">
       <nav className="flex justify-center w-full h-16 border-b border-b-foreground/10">
         <div className="flex items-center justify-between w-full max-w-4xl p-3 text-sm">
           <Logo />
-          {isSupabaseConnected && <AuthButton />}
+          <AuthButton />
         </div>
       </nav>
 
@@ -35,7 +17,7 @@ export default async function Index() {
         <Header />
         <main className="flex flex-col flex-1 gap-6">
           <h2 className="mb-4 text-4xl font-bold">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          <SignUpUserSteps />
         </main>
       </div>
 
