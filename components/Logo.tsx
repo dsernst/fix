bae6774d1e52906841ebd6fa684a095c594b96f1
David Ernst <git@dsernst.com>
2023-12-19 18:16:51 -0800

    Logo

---
+++ components/Logo.tsx
@@ -0,0 +1,18 @@
+export function Logo() {
+  return (
+    <a className="flex px-3 py-2 no-underline rounded-md" href="/">
+      <svg
+        aria-label="triangle up"
+        role="img"
+        viewBox="0 0 74 64"
+        className="w-4 h-4 mr-2"
+      >
+        <path
+          d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z"
+          fill="currentColor"
+        ></path>
+      </svg>
+      Fix.tf
+    </a>
+  )
+}
