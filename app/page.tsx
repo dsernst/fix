2741f4671dc5ac73efd95fa471f9b7c69238a855
David Ernst <git@dsernst.com>
2023-12-19 19:42:19 -0800

    Remove tutorial <SignUpUserSteps />

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Logo } from '../components/Logo'
 import AuthButton from '../components/AuthButton'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
 import Header from '@/components/Header'
 
 export default async function Index() {
@@ -16,23 +15,12 @@ export default async function Index() {
       <div className="flex flex-col flex-1 max-w-4xl gap-20 px-3 opacity-0 animate-in">
         <Header />
         <main className="flex flex-col flex-1 gap-6">
-          <h2 className="mb-4 text-4xl font-bold">Next steps</h2>
-          <SignUpUserSteps />
+          <h2 className="mb-4 text-3xl font-bold">Previous Entries:</h2>
         </main>
       </div>
 
-      <footer className="flex justify-center w-full p-8 text-xs text-center border-t border-t-foreground/10">
-        <p>
-          Powered by{' '}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
+      <footer className="flex justify-center w-full p-8 text-xs text-center border-t text-foreground/20 border-t-foreground/10">
+        <p>Do great things</p>
       </footer>
     </div>
   )
